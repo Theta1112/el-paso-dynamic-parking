e14754eb7fe0ea3daa718bacd0c5549502f5ab0d
Tutut Indriaty <indriaty@upenn.edu>
2025-03-18 15:11:37 -0400

    first screen upload, main and css update
    ok

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 // script.js
+import { initializeFirstScreen } from './first_screen.js';
 import { initializeMap } from './map_create.js';
 import { loadMeterData, loadStreetData } from './data_load.js';
 import { initializeStreets } from './street_create.js';
@@ -15,6 +16,8 @@ var map = initializeMap(document.querySelector('#map'), eventBus);
 const meterData = await loadMeterData(eventBus);
 const streetData = await loadStreetData(eventBus);
 
+
+initializeFirstScreen();
 
 // Render street and meter layers
 initializeStreets(map, meterData, streetData, eventBus);
