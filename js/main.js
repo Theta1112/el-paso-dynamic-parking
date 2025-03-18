b6eb787794a491ee3cb99cc2fbba7cf21e0b647c
SeanMKoh <seankoh154@gmail.com>
2025-03-18 15:02:49 -0400

    fixed main to reflect changes to slider

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 // script.js
+import { initializeFirstScreen } from './first_screen.js';
 import { initializeMap } from './map_create.js';
 import { loadMeterData, loadStreetData } from './data_load.js';
 import { initializeStreets } from './street_create.js';
-import { initializeSlider } from './slider.js';
+import { initializeSideSlider } from './slider.js';
+
 
 // Event bus
 const eventBus = new EventTarget(); 
@@ -14,8 +16,12 @@ var map = initializeMap(document.querySelector('#map'), eventBus);
 const meterData = await loadMeterData(eventBus);
 const streetData = await loadStreetData(eventBus);
 
+
+initializeFirstScreen();
+
 // Render street and meter layers
 initializeStreets(map, meterData, streetData, eventBus);
 
 // Enable slider button
-initializeSlider(document.querySelector('time-slider'), eventBus);
+initializeSideSlider(document.querySelector('.time-slider'), eventBus);
+initializeSideSlider(document.querySelector('.data-slider'), eventBus);
