13c5d95d2c3e4f9040286d3feba92112db60a0f3
SeanMKoh <seankoh154@gmail.com>
2025-04-08 17:51:37 -0400

    added heatmap

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // script.js
 import { initializeFirstScreen } from './first_screen.js';
 import { initializeMap } from './map_create.js';
-import { loadDurationData, loadMeterData, loadOccupancyData, loadStreetData } from './data_load.js';
+import { loadDurationData, loadHistoryData, loadMeterData, loadOccupancyData, loadStreetData } from './data_load.js';
 import { initializeStreets } from './street_create.js';
 import { initializeSideSlider } from './slider.js';
 import { initializeGraph } from './graph_hist.js';
@@ -19,7 +19,9 @@ const meterData = await loadMeterData(eventBus);
 const streetData = await loadStreetData(eventBus);
 const durationData = await loadDurationData(eventBus);
 const occupancyData = await loadOccupancyData(eventBus);
+const historyData = await loadHistoryData(eventBus);
 
+console.log(historyData)
 
 initializeFirstScreen();
 
@@ -28,7 +30,7 @@ initializeStreets(map, meterData, streetData, eventBus);
 console.log(occupancyData)
 
 initializeGraph(document.querySelector('#durationhist'), durationData, eventBus);
-initializeHeatmap(document.querySelector('#occupancyheatmap', occupancyData, eventBus));
+initializeHeatmap(document.querySelector('#occupancyheatmap'), occupancyData, eventBus);
 // Enable slider button
 //initializeSideSlider(document.querySelector('.time-slider'), eventBus);
 //initializeSideSlider(document.querySelector('.data-slider'), eventBus);
