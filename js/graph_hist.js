e6442ffcbb5fd531cbeb027cd6ffccc9c38e175a
SeanMKoh <seankoh154@gmail.com>
2025-04-18 11:08:23 -0400

    dynamic histogram
    allowed histogram to show dynamic data

--- js/graph_hist.js
+++ js/graph_hist.js
@@ -1,30 +1,9 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
-function initializeHistogram(graphEl, data, eventBus) {
+function initializeHistogram(graphEl, fullData, eventBus) {
 
-  console.log(data)
-  //renderGraph(graphEl, data, eventBus);
-
-  eventBus.addEventListener('filter-change', (e) => {
-    const filteredData = data.filter((row) => {
-
-      //console.log(e)
-      return(
-        (e.detail.month == "all" || e.detail.month == row.month)
-        & (e.detail.cluster == "all" || e.detail.month == row.cluster)
-      )
-    })
-
-    //console.log(data)
-
-    renderGraph(graphEl, filteredData, eventBus);
-  })
-} 
-
-function renderGraph(graphEl, rawData, eventBus) {
-
-  const margin = {top: 10, right: 10, bottom: 25, left: 20}
-  const width = 300 - margin.left - margin.right
+  const margin = {top: 10, right: 10, bottom: 25, left: 50}
+  const width = 350 - margin.left - margin.right
   const height = 200;
   const xAxisLabelHeight = 20;
   const yAxisLabelHeight = 20;
@@ -36,17 +15,37 @@ function renderGraph(graphEl, rawData, eventBus) {
   const graphHeight = height - xAxisLabelHeight - yAxisLabelHeight - margin.bottom - margin.top - floatHeight;
   const graphBottom = yAxisLabelHeight + graphHeight + margin.top;
 
+  // Define data containers
+  var data, dataCumulative, dataPercentage;
 
-  // Represent quantities in 1000s
-  rawData.map((d) => {d.qty = Math.floor(d.qty / 1000)})
-  
-  const data = rawData.slice(0,-1)
+  // Process the data to make it ready for rendering
+  function prepareData(raw){
 
-  const cumSum = (sum => value => sum += value)(0);
+    // Cumsum function for data preperation
+    const cumSum = (sum => value => sum += value)(0);
 
-  const dataCumulative = rawData.map(d => d.qty).map(cumSum)
+    // Represent quantities in 1000s
+    const scaled = raw.map((d) => {
+      const out = d
+      out.qty = Math.floor(d.qty)
+      return(out)
+    })
 
-  const dataPercentage = dataCumulative.map(d => Math.round(d / Math.max(...dataCumulative) * 1000) / 10)
+    //console.log(scaled)
+    
+    data = scaled.slice(0,-1)
+
+    //console.log(data)
+
+    dataCumulative = scaled.map(d => d.qty).map(cumSum)
+
+    dataPercentage = dataCumulative.map(d => Math.round(d / Math.max(...dataCumulative) * 1000) / 10)
+  }
+
+  // Execute data preperation
+  prepareData(fullData);
+
+  //console.log(data)
 
   // append the svg object to the body of the page
   var svg = d3.create("svg")
@@ -64,14 +63,13 @@ function renderGraph(graphEl, rawData, eventBus) {
 
   // Add Y axis
   var y = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.qty)])
     .range([ graphBottom, graphBottom - graphHeight]);
 
-  svg.append("g")
+  var yAxis = svg.append("g")
     .attr("transform", `translate(${margin.left - 5},0)`)
-    .call(d3.axisLeft(y).tickValues([100,200,300,400,500]).tickSizeOuter(0));
 
   // Bars
+  // Initialize it at 0
   svg.selectAll("mybar")
     .data(data)
     .enter()
@@ -84,17 +82,15 @@ function renderGraph(graphEl, rawData, eventBus) {
       .attr("height", function(d) { return graphBottom - y(0); }) // always equal to 0
       .attr("y", function(d) { return y(0); })
       .on('mouseover', function (d, i) {
-        
         // Dispatch event when bar is hovered over
-        //console.log(this.getAttribute('bucket'))
         const barHovered = new CustomEvent('bar-hover', { detail: { bucket: this.getAttribute('bucket') }});
-        eventBus.dispatchEvent(barHovered); })
-
+        eventBus.dispatchEvent(barHovered); 
+      })
       .on('mouseout', function (d, i) {
-        
         // Dispatch coresponding even when bar is hovered off
         const barHoveredOff = new CustomEvent('bar-hover-off');
-        eventBus.dispatchEvent(barHoveredOff); })
+        eventBus.dispatchEvent(barHoveredOff); 
+      })
 
   // x-axis label
   svg.append("text")
@@ -123,57 +119,111 @@ function renderGraph(graphEl, rawData, eventBus) {
     .attr("font-size", 20)
     .style('fill', '#FF5733')
     .text("");
-
-  // Animation
-  svg.selectAll("rect")
-    .transition()
-    .duration(800)
-    .attr("y", function(d) { return y(d.qty); })
-    .attr("height", function(d) { return graphBottom - y(d.qty); })
-    .delay(function(d,i){
-      //console.log(i); 
-      return(i*100)
-    })
   
   graphEl.append(svg.node());
 
-  // Enable highlighting event to show percentage of vehicles at any bucket
-  graphEl.querySelectorAll('rect').forEach((rect) => {
-    // Listen for when another bar is hovered over. If that bar has higher bucket, turn yellow
-    eventBus.addEventListener('bar-hover', (e) => {
+  // Update data and re-render the graph
+  function updateGraph(){ 
 
-      if (parseInt(e.detail.bucket) >= parseInt(rect.getAttribute('bucket'))) {
+    // Adjust y domain to updated data
+    y.domain([0, d3.max(data, d => d.qty)])
+
+    console.log(d3.max(data, d => d.qty))
+
+    yAxis
+      .transition()
+      .duration(400)
+      .call(d3.axisLeft(y).tickSizeOuter(0));
+
+
+    var rects = svg.selectAll("rect")
+      .data(data)  
+
+    // Animation
+    rects
+      .enter()
+      .append("rect") // Add a new rect for each new elements
+      .merge(rects) // get the already existing elements as well
+      .transition()
+      .duration(100)
+      .attr("y", function(d) { return y(d.qty); })
+      .attr("height", function(d) { return graphBottom - y(d.qty); })
+      .delay(function(d,i){
+        //console.log(i); 
+        return(250 + i*50)
+      })
+  }
+
+  // Set up highlighting event to show percentage of vehicles at any bucket
+  function initializeHighlighting(){
+    graphEl.querySelectorAll('rect').forEach((rect) => {
+      // Listen for when another bar is hovered over. If that bar has higher bucket, turn yellow
+      eventBus.addEventListener('bar-hover', (e) => {
+  
+        if (parseInt(e.detail.bucket) >= parseInt(rect.getAttribute('bucket'))) {
+          d3.select(rect).transition()
+            .duration('50')
+            .attr('fill', '	#FF5733'); 
+        }
+      })
+  
+      eventBus.addEventListener('bar-hover-off', (e) => {
         d3.select(rect).transition()
           .duration('50')
-          .attr('fill', '	#FF5733'); 
-      }
+          .attr('fill', "#004080"); 
+      })
     })
+  
+    // Enable text to show the percentage of the bars highlighted
+    graphEl.querySelector('.percentage-text')
+  
+    eventBus.addEventListener('bar-hover', (e) => {
+  
+      var bucket = parseInt(e.detail.bucket);
+  
+      var percentage = dataPercentage[data.map(d => d.bucket).indexOf(bucket)]
+  
 
+      //console.log(dataCumulative)
+      //console.log(percentage)
+  
+      graphEl.querySelector('.percentage-text').innerHTML = percentage + "%"
+    })
+  
     eventBus.addEventListener('bar-hover-off', (e) => {
-      d3.select(rect).transition()
-        .duration('50')
-        .attr('fill', "#004080"); 
+  
+      graphEl.querySelector('.percentage-text').innerHTML = ""
     })
+  
+  }
+
+  initializeHighlighting()
+
+  
+
+  eventBus.addEventListener('filter-change', (e) => {
+    
+    const filterMonth = e.detail.month == "all" ? 0 : e.detail.month 
+    const filterCluster = e.detail.cluster == "all" ? 0 : e.detail.cluster 
+
+    //console.log(filterMonth)
+    //console.log(fullData)
+
+    const filteredData = fullData.filter((row) => filterMonth == row.month & filterCluster == row.cluster)
+  
+    //console.log(filteredData)
+
+    // Reprepare the data again
+    prepareData(filteredData)
+
+    //console.log(data)
+
+    // Render the graph
+    updateGraph()
   })
 
-  // Enable text to show the percentage of the bars highlighted
-  graphEl.querySelector('.percentage-text')
 
-  eventBus.addEventListener('bar-hover', (e) => {
-
-    const bucket = parseInt(e.detail.bucket);
-
-    const percentage = dataPercentage[data.map(d => d.bucket).indexOf(bucket)]
-
-    //console.log(percentage)
-
-    graphEl.querySelector('.percentage-text').innerHTML = percentage + "%"
-  })
-
-  eventBus.addEventListener('bar-hover-off', (e) => {
-
-    graphEl.querySelector('.percentage-text').innerHTML = ""
-  })
+  
 
 }
 
